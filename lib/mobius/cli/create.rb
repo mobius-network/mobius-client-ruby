c52e2bb9f857c78d054942b51469fcd57ee2d9b0
Viktor Sokolov <gzigzigzeo@gmail.com>
2018-03-27 21:05:59 +0400

    Add cosigner task

--- lib/mobius/cli/create.rb
+++ lib/mobius/cli/create.rb
@@ -3,6 +3,8 @@ require "net/http"
 
 class Mobius::Cli::Create < Thor
   desc "dapp_account", "Create DApp Store account funded with MOBI and XLM"
+  method_option :application, type: :string, aliases: "-a"
+  # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
   def dapp_account
     say "Calling Mobius FriendBot..."
     keypair = Stellar::KeyPair.random
@@ -10,9 +12,14 @@ class Mobius::Cli::Create < Thor
     say " * Public Key: #{keypair.address}"
     say " * Private Key: #{keypair.seed}"
     say " * MOBI balance: #{Mobius::Account.new(keypair).balance}"
+    if options["application"]
+      say "Adding cosigner..."
+      add_cosigner(keypair, options["application"])
+    end
   rescue StandardError => e
     say "[ERROR] #{e.message}", :red
   end
+  # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
 
   desc "account", "Create regular Stellar account funded with XLM only"
   def account
@@ -22,5 +29,12 @@ class Mobius::Cli::Create < Thor
     say " * Public Key: #{keypair.address}"
     say " * Private Key: #{keypair.seed}"
     say " * XLM balance: #{Mobius::Client::Blockchain::Account.new(keypair).balance(:native)}"
+  end
+
+  no_commands do
+    def add_cosigner(keypair, cosigner)
+      cosigner_keypair = Mobius::Client.to_keypair(cosigner)
+      Mobius::Client::Blockchain::AddCosigner(keypair, cosigner_keypair)
+    end
   end
 end

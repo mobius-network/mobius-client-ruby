997c485d2697b0d5b6876f3b64479bbf35a14706
Viktor Sokolov <gzigzigzeo@gmail.com>
2018-03-28 20:11:44 +0400

    Challenge/Auth tasks draft

--- lib/mobius/cli/auth.rb
+++ lib/mobius/cli/auth.rb
@@ -9,4 +9,19 @@ class Mobius::Cli::Auth < Thor
     Mobius::Client::Blockchain::AddCosigner.call(user_keypair, app_keypair)
     say "#{app_keypair.address} is now authorized to withdraw from #{user_keypair.address}"
   end
+
+  # desc "challenge <App public> <URL>", "Obtain challenge transaction from application and verify it"
+  # def challenge(app_public)
+  #   say "Challenge transaction XDR:"
+  #   say Mobius::Client::Auth::Challenge.call(user_seed, 3600 * 24)
+  # end
+  #
+  # desc "auth <User secret> <App public> <URL>", "Obtain auth token from application"
+  # def auth(user_seed, app_public, url)
+  #   say "Challenge transaction XDR:"
+  #   say Mobius::Client::Auth::Challenge.call(user_seed, 3600 * 24)
+  #
+  #   say "POST #{url}..."
+  #   say
+  # end
 end
